6a2ab146fa7c101b984151d975f787232529aee3
admin <admin@DESKTOP-T1K1O2B>
2017-03-20 14:18:32 +0800

    添加校验组织机构代码的程序

---
+++ Credit_Investigation/src/check/CheckORG.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package check;
+
+/**
+ * @author 杨少博
+ *2017-3-20下午2:17:54
+ *校验组织机构代码的程序
+ */
+public class CheckORG {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+
+	}
+
+}
