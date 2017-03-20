593e1ace72898944275162a5288478fab77c0c86
admin <admin@DESKTOP-T1K1O2B>
2017-03-20 14:25:13 +0800

    完善校验程序

--- Credit_Investigation/src/check/CheckORG.java
+++ Credit_Investigation/src/check/CheckORG.java
@@ -3,6 +3,8 @@
  */
 package check;
 
+import java.util.regex.Pattern;
+
 /**
  * @author 杨少博
  *2017-3-20下午2:17:54
@@ -15,6 +17,16 @@ public class CheckORG {
 	 */
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
+		String code="74794680-6";
+		if(code.equals("00000000-0")){
+			System.out.println("组织机构代码有问题");
+			return;
+		}
+		if(!Pattern.matches("[A-Za-z0-9]{8}-[A-Za-z0-9]",code)){
+			System.out.println("组织机构代码不符合规范");
+			return;
+		}
+		System.out.println("组织机构代码符合校验规则");
 
 	}
 
